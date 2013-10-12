28bc7be7467bf1e8fd41536d78a9993e700db38d
RemyD <remyd@civx.us>
2013-10-12 15:47:19 -0400

    Added the main app

---
+++ run.py
@@ -0,0 +1,28 @@
+"""
+Author: Remy D <remyd@civx.us>
+        Ralph Bean <rbean@redhat.com>
+License: Apache 2.0
+"""
+import os
+
+from flask import Flask
+from flask.ext.mako import MakoTemplates, render_template
+
+app = Flask(__name__)
+app.template_folder = "templates"
+mako = MakoTemplates(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html', name='mako')
+
+
+
+if __name__ == "__main__":
+    if 'OPENSHIFT_PYTHON_IP' in os.environ:
+        host = os.environ['OPENSHIFT_PYTHON_IP']
+        port = int(os.environ['OPENSHIFT_PYTHON_PORT'])
+        app.run(host=host, port=port)
+    else:
+        app.debug = True
+        app.run()
