e8d1094c6cdd9fd8540ced06a3420136f3954181
Mike Nolan <me@michael-nolan.com>
2013-10-12 16:38:31 -0400

    updated js to display multiple lines

--- static/js/script.js
+++ static/js/script.js
@@ -1,22 +1,34 @@
+var directionsService = new google.maps.DirectionsService();
+
 function initialize() {
 	var mapOptions = {
 		zoom: 8,
 		center: new google.maps.LatLng(-34.397, 150.644),
 		mapTypeId: google.maps.MapTypeId.ROADMAP
 	}
-	var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions),
-		url = 'http://maps.googleapis.com/maps/api/directions/json?origin=PENFIELD RD ROCHESTER NY&destination=DALE RD ROCHESTER NY&sensor=false';
-	$.getJSON(url, function (data) {
-		console.log(data, map);
-	});
-}
-
-function displayDirections(data, map) {
-	var directionsDisplay = new google.maps.DirectionsRenderer();
-	directionsDisplay.setMap(map);
+	var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 
 	var start = 'PENFIELD RD ROCHESTER NY',
-		end = 'DALE RD ROCHESTER NY';
+		end = 'DALE RD ROCHESTER NY',
+		start2 = 'CLOVER ST ROCHESTER NY',
+		end2 = 'ALLENS CREEK ROCHESTER NY';
+
+	displayDirections(map, start, end);
+	displayDirections(map, start2, end2);
+}
+
+function displayDirections(map, start, end) {
+	var polylineOptionsActual = {
+		strokeColor: '#FF0000',
+		strokeOpacity: 1.0,
+		strokeWeight: 5
+	};
+	var directionsDisplay = new google.maps.DirectionsRenderer(
+		{
+			suppressMarkers: true, 
+			polylineOptions: polylineOptionsActual
+		});
+	directionsDisplay.setMap(map);
 
 	var request = {
 		origin: start,
@@ -24,7 +36,13 @@ function displayDirections(data, map) {
 		travelMode: google.maps.TravelMode.DRIVING
 	}
 
-	
+	directionsService.route(request, function (result, status) {
+		console.log('status: ', status)
+		if (status == google.maps.DirectionsStatus.OK) {
+			console.log('OK');
+			directionsDisplay.setDirections(result);
+		}
+	});	
 }
 
 
