eb0226705f21dbf347934c91c1e4fb1df50e4ea2
RemyD <remyd@civx.us>
2013-10-13 11:24:52 -0400

    Added routes for preso

--- run.py
+++ run.py
@@ -7,6 +7,7 @@ import os
 
 from flask import Flask
 from flask.ext.mako import MakoTemplates, render_template
+from flask import redirect, url_for
 
 app = Flask(__name__)
 app.template_folder = "templates"
@@ -20,6 +21,17 @@ def index():
 @app.route('/about')
 def about():
     return render_template('about.mak', name='mako')
+
+
+@app.route('/story')
+def story():
+    return render_template('story.mak', name='mako')
+    #return redirect(url_for('static', filename='img/preso.svg'))
+
+
+@app.route('/slides')
+def slides():
+    return redirect(url_for('static', filename='img/preso.svg'))
 
 
 
