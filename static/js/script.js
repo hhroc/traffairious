82885c344792f209a9b2648e820e23d596b57648
mpn3712 <mpn3712@rit.edu>
2013-12-05 20:00:07 -0500

    Added map

--- static/js/script.js
+++ static/js/script.js
@@ -1,2 +1,13 @@
-var map = L.map('#map-canvas').setView([lat, lng], zoom);
+var map_container = document.getElementById('map-canvas');
 
+var map = L.map('map-canvas', {
+	center: [43.1850, -77.6115],
+	zoom: 12,
+	layers: [
+	 new L.TileLayer('http://otile{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png', {
+                attribution: 'Tiles Courtesy of <a href="http://www.mapquest.com/">MapQuest</a> &mdash; Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
+                maxZoom: 18,
+                subdomains: '1234'
+    	})
+	 ]
+})
