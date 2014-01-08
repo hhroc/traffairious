58c600aa3c482a0628f4490b9db38e5e36e1f8e2
thequbit <thequbit@gmail.com>
2014-01-08 17:42:04 -0500

    pulled latest data from DOT data and placed into a single column within a json object

---
+++ tools/distill_dot_data.py
@@ -0,0 +1,70 @@
+import json
+
+def readcolumns(data):
+    columns = []
+    #print data[1]
+    for i in range(0,len(data[0])):
+        name = data[0][i].strip()
+        if len(name.strip()) == 4: #data[1][i].replace('-','').strip():
+            isnum = True
+        else:
+            isnum = False
+        #print "'{0}' is '{1}'".format(name,isnum)
+        columns.append((name,isnum))
+    return columns
+
+def readdata(infile):
+    data = []
+    with open(infile,'r') as f:
+        lines = f.readlines()
+        i = 0
+        for line in lines:
+            #if i == 0:
+            #    columns = []
+            #    for item in line.split('\t'):
+            #        if item
+            #        columns.append((item,isnum))
+            #else:
+                items = []
+                for item in line.split('\t'):
+                    items.append(item.strip())
+                data.append(items)
+        i += 0
+    #print data[0][1:10]
+    #print data[1][1:10]
+    return data
+
+if __name__ == '__main__':
+
+
+    infile = "../data/HistoricTrafficData1977to2011.csv"
+    print "Reading Data ..."
+    rows = readdata(infile)
+
+    print "Reading Column Headers ..."
+    columns = readcolumns(rows)
+
+    rows = rows[1:]
+    #print "{0}\n".format(rows[0])
+    #print "{0}\n".format(rows[1])
+
+    validcols = [0,1,2,3,4,5,6,7,8,9,10]
+
+    print "Processing Data ..."
+    results = []
+    for row in rows:
+        result = {}
+        for i in range(0,len(validcols)):
+            result[columns[i]] = row[i]
+        volume = 0;
+        for i in range(13,47):
+            if row[i].strip() != '':
+                volumn = row[i]
+                break
+        result['traffic_volume'] = volume;
+
+    with open("../data/distilled_dot_data.json","w") as f:
+        f.write(json.dumps(results))
+
+    print "File Processed Successfully."
+
