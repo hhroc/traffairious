0d6fa97a3a4cc859a46beddd22c348c07a08320d
Mihir Singh <me@mihirsingh.com>
2014-01-22 18:14:32 -0500

    Show towns in Monroe County on load; don't wait for the user to select Monroe #29

--- static/js/script.js
+++ static/js/script.js
@@ -75,21 +75,16 @@ function loadCounties() {
                 },
                 onEachFeature: function (feature, layer) {
                     counties.addLayer(layer);
-                    layer.on('click', function (event) {
-                        window.setTimeout(function () {
-                            map.setZoom(10);
-                        }, 800);
-                        map.panTo([event.latlng.lat, event.latlng.lng]);
-                        console.log(event);
-                        var name = feature.properties.NAMELSAD.toLowerCase().trim().replace(/\s/g,'_').replace('.', '');
-                        var county_url = 'static/data/counties/' + name + "_towns.json",
-                            school_url = 'static/data/schools/'  + name + ".json";
-                        $.getJSON(county_url, function (data) {
-                            loadTowns(data, layer);
-                        });
-                        $.getJSON(school_url, function (data) {
-                            loadSchools(data, layer);
-                        });
+                    map.setZoom(10);
+
+                    var name = feature.properties.NAMELSAD.toLowerCase().trim().replace(/\s/g,'_').replace('.', '');
+                    var county_url = 'static/data/counties/' + name + "_towns.json",
+                        school_url = 'static/data/schools/'  + name + ".json";
+                    $.getJSON(county_url, function (data) {
+                        loadTowns(data, layer);
+                    });
+                    $.getJSON(school_url, function (data) {
+                        loadSchools(data, layer);
                     });
                     layer.on('mouseover', function (event) {
                         layer.setStyle({ fillColor: 'red' });
